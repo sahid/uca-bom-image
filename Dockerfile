ea588a84c9cbbffb9082b7d7cbdc10450e7d9bd9
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@canonical.com>
2019-09-26 07:59:29 +0000

    export env + add dyr mode + add pkg schroot

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ RUN apt-get update
 RUN apt-get install software-properties-common apt-transport-https -y
 RUN add-apt-repository ppa:ubuntu-cloud-archive/tools -y
 RUN apt-get update
-RUN apt-get install cloud-archive-utils -y
+RUN apt-get install schroot cloud-archive-utils -y
 
 COPY . /home/ubuntu/workdir
--- bom.sh
+++ bom.sh
@@ -3,11 +3,14 @@
 set -xe
 
 export LANG="en_US.UTF-8"
+export LANGUAGE="en_US:en"
+export LC_ALL="en_US"
 
 # Should be set from ENV
 #TARGET="trusty-mitaka"
 #OS_SERIES="folsom"
 #TESTING_PPA="ppa:openstack-ubuntu-testing/folsom-stable-testing"
+#ONLY_BUILD=1 t run is dry mode.export 
 # end
 
 staging_ppa="ppa:ubuntu-cloud-archive/$OS_SERIES-staging"
@@ -19,10 +22,12 @@ for pkg in ${packages[*]}; do
     }
     DEB_BUILD_OPTIONS=nostrip sbuild-$OS_SERIES -n -A ${pkg}*/*.dsc && {
         debsign -k9935ACDC ${pkg}*/*_source.changes
-        dput -f $staging_ppa ${pkg}*/*_source.changes
-        dput -f $TESTING_PPA ${pkg}*/*_source.changes
-        reprepro --waitforlock 10 -b ${HOME}/www/apt includedeb $TARGET *.deb
-        rm -f *.deb *.changes
+	if [ -n $ONLY_BUILD ]
+	then
+            dput -f $staging_ppa ${pkg}*/*_source.changes
+            dput -f $TESTING_PPA ${pkg}*/*_source.changes
+            reprepro --waitforlock 10 -b ${HOME}/www/apt includedeb $TARGET *.deb
+	fi
     } || {
         echo "Failed to build $pkg - retry later" >> failed.txt
     }
